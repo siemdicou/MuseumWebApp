c79dc441b089a95aaddee876f7fddd4f9c5d412a
siemdicou <siemdicou@hotmail.com>
2015-11-09 13:55:48 +0100

    hi

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ window.addEventListener("load", function(){
 
     /** we maken de notificationBar aan, gekoppeld aan het HTML element */
     var notificationBar = new NotificationBar(".notification-bar");
+    var	menu get
 
     /** we laten een begin notificatie zien */
     notificationBar.showMessage("hoi");

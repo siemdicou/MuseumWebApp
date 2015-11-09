ac46ea5bc8fdfe9cbcb5321667163749aea3fd83
Natan143 <Natanek@mbp-natanek.media.local>
2015-11-09 14:26:22 +0100

    v.1

--- js/main.js
+++ js/main.js
@@ -5,11 +5,11 @@ window.addEventListener("load", function(){
     var	menu get
 
     /** we laten een begin notificatie zien */
-    notificationBar.showMessage("hoi");
+    // notificationBar.showMessage("hoi");
 
     /** we checken of je op de pagina klikt */
     document.addEventListener("click", function(){
-        notificationBar.showMessage(notificationBar);
+        notificationBar.show(200);
     });
 
 });

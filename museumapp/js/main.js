c8212ad8dee5cd954af7cea07aadb1606982423b
joey1006 <joeysteffens@quicknet.nl>
2015-11-10 09:00:09 +0100

    t

---
+++ museumapp/js/main.js
@@ -0,0 +1,21 @@
+window.addEventListener("load", function(){
+
+    /** we maken de notificationBar aan, gekoppeld aan het HTML element */
+    var notificationBar = new NotificationBar(".notification-bar");
+
+    /** we laten een begin notificatie zien */
+    // notificationBar.showMessage("hoi");
+
+    /** we checken of je op de pagina klikt */
+    document.addEventListener("click", function(){
+        notificationBar.show();
+    });
+
+
+    document.addEventListener("click", function(){
+        notificationBar.delete();
+    });
+
+
+
+});
